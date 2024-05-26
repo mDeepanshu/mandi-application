12da2589147f924069df3ad7e97691caacb5fea9
mdeepanshu <deepanshumalviya8964@gmail.com>
2024-05-26 15:37:34 +0530

    root element fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBI-TlWQnGAD6EzOw-2Td0ScXI8g3LZTgA",
+  authDomain: "mandi-application.firebaseapp.com",
+  projectId: "mandi-application",
+  storageBucket: "mandi-application.appspot.com",
+  messagingSenderId: "491088522122",
+  appId: "1:491088522122:web:163ef9fe2e8f05f0def673"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
