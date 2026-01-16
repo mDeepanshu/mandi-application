ea36cab811ef5ca6eb4936913875cbecc64598a3
mdeepanshu <deepanshumalviya8964@gmail.com>
2026-01-16 22:35:08 +0530

    item post api

--- src/gateway/item-master-apis.js
+++ src/gateway/item-master-apis.js
@@ -2,7 +2,7 @@ import axiosHttp from "../interceptors/error-handling-interceptor";
 
 export const addItemGlobal = async (data) => {
     try {
-        const response = await axiosHttp.post('/', data);
+        const response = await axiosHttp.post('/item', data);
         return response.data;
     } catch (error) {
         console.error('Error posting data:', error);
